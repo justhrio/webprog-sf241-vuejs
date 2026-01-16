d7a1d61ba71fe3a95c801771ed762ec069cb1d9d
Xander Revelar <xsrevelar@student.apc.edu.ph>
2026-01-16 16:22:50 +0800

    fix

--- home/js/script.js
+++ home/js/script.js
@@ -129,3 +129,21 @@ createApp({
         }, 100); 
     }
 }).mount('#vue-app');
+
+const app = Vue.createApp({
+   data() {
+    return {
+      manyFoods: [
+        'img_burrito.svg',
+        'img_salad.svg',
+        'img_cake.svg',
+        'img_soup.svg',
+        'img_fish.svg',
+        'img_pizza.svg',
+        'img_rice.svg'
+      ]
+    }
+   }
+  })
+  
+  app.mount('#app')
