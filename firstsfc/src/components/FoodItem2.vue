5facd06cad647f334990b709269ba5516e241501
Xander Revelar <xsrevelar@student.apc.edu.ph>
2026-01-20 02:51:14 +0000

    now this time added the file

---
+++ firstsfc/src/components/FoodItem2.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>{{ name }}</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
+ 
+<script>
+export default {
+  data() {
+    return {
+      name: 'Lemon zest',
+      message: 'yeah'
+    }
+  }
+};
+</script>
+ 
+<style></style>
