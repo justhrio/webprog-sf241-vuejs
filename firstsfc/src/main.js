e5cdc78b0c3da6ef4bbd707c95dffcb487599e9e
Xander Revelar <xsrevelar@student.apc.edu.ph>
2026-01-30 12:44:29 +0800

    deleted some lines in main js

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -1,18 +1,12 @@
 import { createApp } from 'vue'
 
 import App from './App.vue'
-import FoodItem from './components/FoodItem.vue'
-import FoodItem2 from './components/FoodItem2.vue'
-// 1. Import the new file
-import PersonalProfile from './components/PersonalProfile.vue' 
+
 import CommentForm from './components/CommentForm.vue'
 import Comment from './components/Comment.vue'
 
 const app = createApp(App)
 
-app.component('food-item', FoodItem)
-app.component('food-item2', FoodItem2)
-app.component('personal-profile', PersonalProfile) 
 app.component('comment-form', CommentForm)
 app.component('comment', Comment)
 app.mount('#app')
